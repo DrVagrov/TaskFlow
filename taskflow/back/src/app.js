918478d2edb54a2de9c66a42bde184ddb98bc25d
DrVagrov <72596408+DrVagrov@users.noreply.github.com>
2026-03-18 14:25:21 +0100

    app.js + env.exemple

--- taskflow/back/src/app.js
+++ taskflow/back/src/app.js
@@ -16,7 +16,7 @@ const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin : ['http://localhost:3000',("https://"+FONT_PUBLIC_DOMAIN)]}));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
